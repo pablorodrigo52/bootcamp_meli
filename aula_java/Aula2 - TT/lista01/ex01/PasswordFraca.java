a3f22b191e9d0a55690e056c486f70e5cf129099
Pablo Rodrigo Lima dos Santos <pablo.santos@mercadolivre.com>
2021-06-29 14:54:30 -0300

    feat: aula2 tt ex01

---
+++ aula_java/Aula2 - TT/lista01/ex01/PasswordFraca.java
@@ -0,0 +1,8 @@
+
+
+public class PasswordFraca extends Senha{
+    public PasswordFraca() {
+        // Senhas com no mínimo 5 números
+        super("^(?=.*[0-9])(?=.*[a-z]).{2,128}$");
+    }
+}
---
+++ aula_java/Aula2 - TT/lista01/ex01/PasswordMedia.java
@@ -0,0 +1,8 @@
+
+
+public class PasswordMedia extends Senha{
+
+    public PasswordMedia(){
+            super("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{4,128}$");
+    }
+}
